98fa4901bb6a4a2d2ebc113896729c0168644025
Username-Taken45 <57226911+Username-Taken45@users.noreply.github.com>
2020-02-28 17:16:13 +0100

    Add files via upload

---
+++ dicemodule.py
@@ -0,0 +1,35 @@
+import random 
+
+
+r1 = 0
+r2 = 0
+r3 = 0
+r4 = 0
+r5 = 0
+r6 = 0
+roll = 0
+
+
+def roll():
+    i = int(input() )
+    while i > 0:
+
+        roll = random.randint(1, 6)
+        if roll == 1:
+            r1 = r1 +1
+        if roll == 2:
+           r2 = r2 + 1
+        if roll == 3:
+          r3 = r3 + 1
+        if roll == 4:
+          r4 = r4 + 1
+        if roll == 5:
+          r5 = r5 + 1
+        if roll == 6:
+          r6 = r6 + 1
+        print(r1, r2, r3, r4, r5, r6)
+        i = i -1
+
+
+print(r1, r2, r3, r4, r5, r6)
+roll()
